e6d48e7475c2a4dce0cd5e0c91e4932456999b59
Charles Moulliard <ch007m@gmail.com>
2015-06-01 15:23:42 +0200

    Add missing _ fot the var GIT_SERVER_PORT. Fix #2

--- bin/conf-and-run-gerrit.sh
+++ bin/conf-and-run-gerrit.sh
@@ -2,36 +2,42 @@
 
 set -e
 
-# Initialize gerrit & reindex the site if the directory doesn't exist
-if [ ! -d "$GERRIT_HOME/site" ]; then
-
-  mkdir -p site/plugins
-  
-  echo ">> Site doesn't exist. We will start gerrit to generate it"
-  java -jar ${GERRIT_HOME}/$GERRIT_WAR init --install-plugin=replication --install-plugin=download-commands --batch -d ${GERRIT_HOME}/site
+# Initialize gerrit & reindex the site if the gerrit-configured doesn't exist
+if [ -f $GERRIT_SITE/.gerrit-configured ]; then
+  echo ">> Gerrit has been configured, then will not generate a new setup"
+else
+  echo ">> .gerrit-configured doesn't exist. We will start gerrit to generate it"
+  java -jar ${GERRIT_HOME}/$GERRIT_WAR init --install-plugin=replication --install-plugin=download-commands --batch -d ${GERRIT_SITE}
   java -jar ${GERRIT_HOME}/$GERRIT_WAR reindex -d ${GERRIT_HOME}/site
-  
+
   # Download Gerrit plugin
   echo ">> Download gerrit plugins - delete project <<"
-  curl -sSL https://ci.gerritforge.com/view/Plugins-stable-2.11/job/Plugin_delete-project_stable-2.11/lastSuccessfulBuild/artifact/target/delete-project-2.11.jar -o ${GERRIT_HOME}/site/plugins/delete-project.jar
+  curl -sSL https://ci.gerritforge.com/view/Plugins-stable-2.11/job/Plugin_delete-project_stable-2.11/lastSuccessfulBuild/artifact/target/delete-project-2.11.jar -o ${GERRIT_SITE}/plugins/delete-project.jar
 
   # Copy our config files
-  cp bin/gerrit.config ${GERRIT_HOME}/site/etc/gerrit.config
-  cp bin/replication.config ${GERRIT_HOME}/site/etc/replication.config
+  cp bin/gerrit.config ${GERRIT_SITE}/etc/gerrit.config
+  cp bin/replication.config ${GERRIT_SITE}/etc/replication.config
   
   # Configure Git Replication
-  sed -i  's/__GIT_SERVER_IP__/'${GIT_SERVER_IP}'/g' ${GERRIT_HOME}/site/etc/replication.config
-  sed -i  's/__GIT_SERVER_PORT__/'${GIT_SERVER_PORT}'/g' ${GERRIT_HOME}/site/etc/replication.config
-  sed -i  's/__GIT_SERVER_USER__/'${GIT_SERVER_USER}'/g' ${GERRIT_HOME}/site/etc/replication.config
-  sed -i  's/__GIT_SERVER_PASSWORD__/'${GIT_SERVER_PASSWORD}'/g' ${GERRIT_HOME}/site/etc/replication.config
-  sed -i  's/__GIT_SERVER_PROJ_ROOT__/'${GIT_SERVER_PROJ_ROOT}'/g' ${GERRIT_HOME}/site/etc/replication.config
+  echo ">> Configure Git Replication"
+  sed -i  's/__GIT_SERVER_IP__/'${GIT_SERVER_IP}'/g' ${GERRIT_SITE}/etc/replication.config
+  sed -i  's/__GIT_SERVER_PORT__/'${GIT_SERVER_PORT}'/g' ${GERRIT_SITE}/etc/replication.config
+  sed -i  's/__GIT_SERVER_USER__/'${GIT_SERVER_USER}'/g' ${GERRIT_SITE}/etc/replication.config
+  sed -i  's/__GIT_SERVER_PASSWORD__/'${GIT_SERVER_PASSWORD}'/g' ${GERRIT_SITE}/etc/replication.config
+  sed -i  's/__GIT_SERVER_PROJ_ROOT__/'${GIT_SERVER_PROJ_ROOT}'/g' ${GERRIT_SITE}/etc/replication.config
 
   # Configure Gerrit
-  sed -i  's/__AUTH_TYPE__/'${AUTH_TYPE}'/g' ${GERRIT_HOME}/site/etc/gerrit.config
+  echo ">> Configure Git"
+  sed -i  's/__AUTH_TYPE__/'${AUTH_TYPE}'/g' ${GERRIT_SITE}/etc/gerrit.config
+  
+  # Add a .gerrit-configured file
+  echo "Add .gerrit-configured file"
+  touch $GERRIT_SITE/.gerrit-configured
+ 
 fi
 
 # Debug purpose
-# ls -la $GERRIT_HOME/site/db
+# ls -la $GERRIT_HOME/SITE/db
 # ls -la $GERRIT_HOME/site/etc
 # cat /home/gerrit/site/etc/replication.config
 # cat /home/gerrit/site/etc/gerrit.config
@@ -44,11 +50,11 @@ chown -R gerrit:gerrit $GERRIT_HOME
 
 # Error reported when we launch gerrit with the bash script
 # /home/gerrit/site/bin/gerrit.sh: line 429: echo: write error: Permission denied
-# ${GERRIT_HOME}/site/bin/gerrit.sh start
+# ${GERRIT_SITE}/bin/gerrit.sh start
 # 
 # To debug it, run this command after starting the container intereactive mode
 # docker run -it -p 0.0.0.0:8080:8080 -p 127.0.0.1:29418:29418 --name my-gerrit cmoulliard/gerrit:1.0 bash
-# bash -x ${GERRIT_HOME}/site/bin/gerrit.sh start
+# bash -x ${GERRIT_SITE}/bin/gerrit.sh start
 
 echo "Gerrit started using the java cmd : java -jar ${GERRIT_WAR} daemon -d ${GERRIT_SITE}"
 exec java -jar ${GERRIT_WAR} daemon -d ${GERRIT_SITE}
