0c339675306ba31f43029608e1fd0de316ca1189
Charles Moulliard <ch007m@gmail.com>
2015-09-24 21:37:49 +0200

    Switch to env to use Docker Machine

--- scripts/set_docker_env.sh
+++ scripts/set_docker_env.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
 
 # Docker Machine
-# DOCKER_IP_PORT=${1:-192.168.99.100:2376}
-# export DOCKER_CERT_PATH="/Users/chmoulli/.docker/machine/machines/default"
-# export DOCKER_HOST="tcp://$DOCKER_IP_PORT"
-# export DOCKER_MACHINE_NAME="default"
-# export DOCKER_TLS_VERIFY="1"
+DOCKER_IP_PORT=${1:-192.168.99.100:2376}
+export DOCKER_CERT_PATH="/Users/chmoulli/.docker/machine/machines/default"
+export DOCKER_HOST="tcp://$DOCKER_IP_PORT"
+export DOCKER_MACHINE_NAME="default"
+export DOCKER_TLS_VERIFY="1"
 
 # Boot2docker
-DOCKER_IP_PORT=${1:-192.168.59.103:2376}
-export DOCKER_HOST=tcp://$DOCKER_IP_PORT
-export DOCKER_CERT_PATH=/Users/chmoulli/.boot2docker/certs/boot2docker-vm
-export DOCKER_TLS_VERIFY=1
+# DOCKER_IP_PORT=${1:-192.168.59.103:2376}
+# export DOCKER_HOST=tcp://$DOCKER_IP_PORT
+# export DOCKER_CERT_PATH=/Users/chmoulli/.boot2docker/certs/boot2docker-vm
+# export DOCKER_TLS_VERIFY=1
