cb0a5ee5b8a6e76fab9a34ae16b225d0601d216e
Charles Moulliard <ch007m@gmail.com>
2015-09-24 17:27:58 +0200

    Update build script to set docker env

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,17 +2,15 @@
 
 PROJECT_DIR=`pwd`
 
-USER=${1:-cmoulliard} # Username to be used to create the image
-GERRIT_TEMP_DIR=${2:-~/temp/gerrit-site} # Temp dir where we will mount the volume locally
-DOCKER_HOST=${3:-172.28.128.4}
+USER=${2:-cmoulliard} # Username to be used to create the image
+GERRIT_TEMP_DIR=${3:-~/temp/gerrit-site} # Temp dir where we will mount the volume locally
 KEYS_DIR=$PROJECT_DIR/ssh-keys
 ADMIN_KEY=$PROJECT_DIR/ssh-admin-key
+
+. ./scripts/set_docker_env.sh
 
 docker stop gerrit
 docker rm gerrit
 rm -rf $GERRIT_TEMP_DIR
-
-export DOCKER_HOST=tcp://$DOCKER_HOST:2375
-export DOCKER_TLS_VERIFY=
 
 docker build -t $USER/gerrit .
